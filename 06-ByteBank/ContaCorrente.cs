c49ac2268e2d0bd95a87c4dd91ef5be70eab58ea
Lucas Correa <lucas.correa@etaure.srv>
2021-04-15 09:48:39 -0300

    Propriedades (completo)

---
+++ 06-ByteBank/ContaCorrente.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+
+        private double _saldo;
+        public double Saldo
+        {
+            get
+            {
+                return this._saldo;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("_saldo negativo!");
+                    return;
+                }
+
+                _saldo = value;
+                return;
+            }
+        }
+
+
+        public bool Sacar(double valor)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this._saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+            return;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+
+            this._saldo -= valor;
+            contaDestino._saldo += valor;
+            return true;
+        }
+    }
+}
---
+++ 06-ByteBank/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            Cliente cliente = new Cliente();
+            cliente.Nome = "Lucas";
+            cliente.CPF = "222.222.222-22";
+            cliente.Profissao = "Desenvolvedor";
+
+            conta.Saldo = 1000;
+            conta.Titular = cliente;
+
+            Console.WriteLine(conta.Titular.Nome);
+            Console.WriteLine(conta.Saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
