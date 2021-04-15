c49956f0d15e8f639a57e0697864d4e58df9200b
Lucas Correa <lucas.correa@etaure.srv>
2021-04-15 10:40:59 -0300

    Parte 3 completa

---
+++ 07-ByteBank/ContaCorrente.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+
+        public static int TotalDeContasCriadas { get; private set; }
+
+        private int _agencia;
+        public int Agencia 
+        {
+            get
+            {
+                return _agencia;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    return;
+                }
+
+                _agencia = value;
+            }
+        }
+        public int Numero { get; set; }
+
+        private double _saldo;
+        public double Saldo
+        {
+            get
+            {
+                return this._saldo;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Saldo negativo!");
+                    return;
+                }
+
+                _saldo = value;
+                return;
+            }
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+            TotalDeContasCriadas++;
+        }
+
+
+        public bool Sacar(double valor)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this._saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+            return;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+
+            this._saldo -= valor;
+            contaDestino._saldo += valor;
+            return true;
+        }
+    }
+}
